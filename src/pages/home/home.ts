1b95b7e74a03c30a9d2e085c04825837637e272a
Rouz Majlessi <rmajlessi@gmail.com>
2018-06-26 16:25:27 -0700

    Modify home page to chat page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams, Content } from 'ionic-angular';
+import { RoomPage } from '../room/room';
+import * as firebase from 'Firebase';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,40 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild(Content) content: Content;
 
+  data = { type:'', nickname:'', message:'' };
+  chats = [];
+  roomkey:string;
+  nickname:string;
+  offStatus:boolean = false;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.roomkey = this.navParams.get("key") as string;
+    this.nickname = this.navParams.get("nickname") as string;
+    this.data.type = 'message';
+    this.data.nickname = this.nickname;
+  
+    let joinData = firebase.database().ref('chatrooms/'+this.roomkey+'/chats').push();
+    joinData.set({
+      type:'join',
+      user:this.nickname,
+      message:this.nickname+' has joined this room.',
+      sendDate:Date()
+    });
+    this.data.message = '';
+  
+    firebase.database().ref('chatrooms/'+this.roomkey+'/chats').on('value', resp => {
+      this.chats = [];
+      this.chats = snapshotToArray(resp);
+      setTimeout(() => {
+        if(this.offStatus === false) {
+          this.content.scrollToBottom(300);
+        }
+      }, 1000);
+    });
+  }
+    
   }
 
-}
+
