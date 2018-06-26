05ee2e1898eee7a70a29a66e31427a2729915b50
Rouz Majlessi <rmajlessi@gmail.com>
2018-06-26 15:49:37 -0700

    Add constant var to told firebase settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,18 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import * as firebase from 'firebase';
 
 import { HomePage } from '../pages/home/home';
+
+const config = {
+  apiKey: 'YOUR_APIKEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  databaseURL: 'YOUR_DATABASE_URL',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+};
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -12,11 +22,10 @@ export class MyApp {
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    firebase.initializeApp(config);
   }
 }
 
