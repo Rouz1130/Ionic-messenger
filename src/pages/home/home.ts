cfeac884c693d4d03156694407f6704342b95a31
Rouz Majlessi <rmajlessi@gmail.com>
2018-06-26 16:30:43 -0700

    Add constant function for converting firebase response to array

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,67 +11,75 @@ export class HomePage {
 
   @ViewChild(Content) content: Content;
 
-  data = { type:'', nickname:'', message:'' };
+  data = { type: '', nickname: '', message: '' };
   chats = [];
-  roomkey:string;
-  nickname:string;
-  offStatus:boolean = false;
+  roomkey: string;
+  nickname: string;
+  offStatus: boolean = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.roomkey = this.navParams.get("key") as string;
     this.nickname = this.navParams.get("nickname") as string;
     this.data.type = 'message';
     this.data.nickname = this.nickname;
-  
-    let joinData = firebase.database().ref('chatrooms/'+this.roomkey+'/chats').push();
+
+    let joinData = firebase.database().ref('chatrooms/' + this.roomkey + '/chats').push();
     joinData.set({
-      type:'join',
-      user:this.nickname,
-      message:this.nickname+' has joined this room.',
-      sendDate:Date()
+      type: 'join',
+      user: this.nickname,
+      message: this.nickname + ' has joined this room.',
+      sendDate: Date()
     });
     this.data.message = '';
-  
-    firebase.database().ref('chatrooms/'+this.roomkey+'/chats').on('value', resp => {
+
+    firebase.database().ref('chatrooms/' + this.roomkey + '/chats').on('value', resp => {
       this.chats = [];
       this.chats = snapshotToArray(resp);
       setTimeout(() => {
-        if(this.offStatus === false) {
+        if (this.offStatus === false) {
           this.content.scrollToBottom(300);
         }
       }, 1000);
     });
   }
-    
+
   sendMessage() {
-    let newData = firebase.database().ref('chatrooms/'+this.roomkey+'/chats').push();
+    let newData = firebase.database().ref('chatrooms/' + this.roomkey + '/chats').push();
     newData.set({
-      type:this.data.type,
-      user:this.data.nickname,
-      message:this.data.message,
-      sendDate:Date()
+      type: this.data.type,
+      user: this.data.nickname,
+      message: this.data.message,
+      sendDate: Date()
     });
     this.data.message = '';
   }
 
   exitChat() {
-    let exitData = firebase.database().ref('chatrooms/'+this.roomkey+'/chats').push();
+    let exitData = firebase.database().ref('chatrooms/' + this.roomkey + '/chats').push();
     exitData.set({
-      type:'exit',
-      user:this.nickname,
-      message:this.nickname+' has exited this room.',
-      sendDate:Date()
+      type: 'exit',
+      user: this.nickname,
+      message: this.nickname + ' has exited this room.',
+      sendDate: Date()
     });
-  
+
     this.offStatus = true;
-  
+
     this.navCtrl.setRoot(RoomPage, {
-      nickname:this.nickname
+      nickname: this.nickname
     });
   }
+}
 
+export const snapshotToArray = snapshot => {
+  let returnArr = [];
 
+  snapshot.forEach(childSnapshot => {
+      let item = childSnapshot.val();
+      item.key = childSnapshot.key;
+      returnArr.push(item);
+  });
 
-  }
-
+  return returnArr;
+};
 
