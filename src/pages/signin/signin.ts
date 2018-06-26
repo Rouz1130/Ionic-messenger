13515ce6f706fd0ce2318869b0d2945c93b2ef42
Rouz Majlessi <rmajlessi@gmail.com>
2018-06-26 16:05:40 -0700

    Add room and addroom , aslo add fucntion to listen for value change in firevase db

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-// import { RoomPage } from  '../room/room';
+import { RoomPage } from  '../room/room';
 
 
 @IonicPage()
