53d2586f3e0d055c726695d9ff64945fcc75f01f
Rouz Majlessi <rmajlessi@gmail.com>
2018-06-26 15:54:57 -0700

    Add Signin page

---
+++ src/pages/signin/signin.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+// import { RoomPage } from  '../room/room';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-signin',
+  templateUrl: 'signin.html',
+})
+export class SigninPage {
+  data = {nickname:""};
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SigninPage');
+  }
+
+  enterNickname() {
+    this.navCtrl.setRoot(RoomPage, {
+      nickname: this.data.nickname
+    });
+  }
+
+}
